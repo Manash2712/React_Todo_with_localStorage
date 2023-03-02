afc8f5f9c0c2342dfd47e05f95b55c98cfbb45de
Manash2712 <manashchauhan2712@gmail.com>
2023-03-02 23:55:46 +0530

    todo app with local storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
-function App() {
+import Todos from './Components/Todos';
+import TodoForm from './Components/TodoForm';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  //useeffect takes 2 parameters a callback and list of dependecy and its compulsory
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos")
+    if (localTodos) {
+      setTodos(JSON.parse(localTodos))
+    }
+  }, []);
+
+  const addTodos = async todo => {
+    setTodos([...todos, todo])
+  }
+
+  //in dependency keep the values in which if there is change then useeffect needs to run
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const markComplete = id => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid>
+      <h1>Todo with Local Storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoForm addTodos={addTodos} />
+    </Container>
+  )
 }
 
 export default App;
